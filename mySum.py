1c3c29e0d5748487c55e338eb66105c00d10239b
Antonio Esposito <antonio.esposito1@inwind.it>
2023-11-24 16:49:23 +0100

    ho creato la funzione intersect

--- mySum.py
+++ mySum.py
@@ -9,3 +9,11 @@ def mysum(L):
 def times(x,y):
     return x * y
 
+def intersect(seq1, seq2):
+    res = []
+    for x in seq1:
+        if x in seq2:
+            res.append(x)
+    return res
+
+    
