b1d04f8cebad64b4325b672ae9c05a6ff8833985
Antonio Esposito <antonio.esposito1@inwind.it>
2023-11-24 17:49:16 +0100

    add mysum4

--- mySum.py
+++ mySum.py
@@ -6,6 +6,10 @@ def mysum(L):
     
 def mysum2(L):
     return 0 if not L else L[0] + mysum2(L[1:])
+
+def mysum4(L):
+    first, *rest = L
+    return first if len(L) == 1 else L[0] + mysum3(rest)
     
 #print(mysum([1,2,3,4]))
 
